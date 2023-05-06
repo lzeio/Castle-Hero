7092f676136f9a4ee5cd936b99b2b23fc1dadf46
labhamgsg <labham7@gmail.com>
2023-05-06 11:02:54 +0530

    Update Castle.cs

--- Assets/InternalAssets/Scripts/Castle/Castle.cs
+++ Assets/InternalAssets/Scripts/Castle/Castle.cs
@@ -3,21 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class Castle : MonoBehaviour
 {
-
+    public static event Action<int> OnCastleHealthUpdated;
     public int Health;
     public void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent(out StatSystem statSystem))
+        if (other.TryGetComponent(out StatSystem statSystem))
         {
-                Health -= statSystem.health;
-                statSystem.KillCharacter();
-                // GamePlayUIScript.Instance.CastleHealth.GetComponent<TMP_Text>().text = Health+"";
-                Destroy(statSystem.gameObject);
+            Health -= statSystem.health;
+            statSystem.KillCharacter();
+            OnCastleHealthUpdated?.Invoke(Health);
+            Destroy(statSystem.gameObject);
         }
     }
 
-  
+
 }
