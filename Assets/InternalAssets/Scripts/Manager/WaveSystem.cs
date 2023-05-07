637c0c65e3a70c3be03800b2396903feb996635b
labhamgsg <labham7@gmail.com>
2023-05-07 23:46:01 +0530

    Update WaveSystem.cs

--- Assets/InternalAssets/Scripts/Manager/WaveSystem.cs
+++ Assets/InternalAssets/Scripts/Manager/WaveSystem.cs
@@ -37,7 +37,7 @@ public class WaveSystem : MonoBehaviour
             //spawn an enemy
             if (enemiesToSpawn.Count > 0)
             {
-                int spawnlocation = UnityEngine.Random.Range(-1, 5);
+                int spawnlocation = UnityEngine.Random.Range(0, 4);
                 GameObject enemy = (GameObject)Instantiate(enemiesToSpawn[0], spawnLocation[spawnlocation].position, spawnLocation[spawnlocation].rotation); // spawn first enemy in our list
                 enemiesToSpawn.RemoveAt(0); // and remove it
                 spawnedEnemies.Add(enemy);
